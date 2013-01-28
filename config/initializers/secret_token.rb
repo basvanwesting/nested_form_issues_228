91a0f755b823789022470c02dc491f4dfa112029
basvanwesting <basvanwesting@gmail.com>
2013-01-28 16:38:38 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DecoratorBreaksLinkToAdd::Application.config.secret_token = 'b19dcb51903a7c19f0bfa0750267c4d6bde094d2efdee140d0ad372daf4c8f8dad15dad9cf3649386587579f37d7659bf88ea22133e7918867ea518f5811ffc4'
