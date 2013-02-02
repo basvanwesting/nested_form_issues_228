150f210e956ed7b770e982503b2cebf93cdc9fce
basvanwesting <basvanwesting@gmail.com>
2013-02-02 08:41:22 +0100

    breaking spec because of use 'decorates_association :books'

--- app/decorators/library_decorator.rb
+++ app/decorators/library_decorator.rb
@@ -1,6 +1,6 @@
 class LibraryDecorator < Draper::Decorator
   delegate_all
-  #decorates_association :books
+  decorates_association :books
 
   # Define presentation-specific methods here. Helpers are accessed through
   # `helpers` (aka `h`). You can override attributes, for example:
